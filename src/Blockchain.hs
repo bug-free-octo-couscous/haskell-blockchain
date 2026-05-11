aebee00d5274697d38990481356efe5e206e0de7
jihoo <jihoop0620@gmail.com>
2026-05-11 19:59:12 +0900

    add Proof Of Work

--- src/Blockchain.hs
+++ src/Blockchain.hs
@@ -5,7 +5,6 @@ import Crypto.Hash.SHA256 (hash)
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Base16 as B16
 
-
 type Hash = String
 
 data Block = Block
@@ -13,50 +12,74 @@ data Block = Block
   , blockTimestamp :: UTCTime
   , blockData      :: String
   , blockPrevHash  :: Hash
+  , blockNonce     :: Int
   , blockHash      :: Hash
   } deriving (Show, Eq)
 
 type Blockchain = [Block]
 
-calculateHash :: Int -> String -> String -> String -> Hash
-calculateHash index timestamp dat prevHash = 
-    let content = show index ++ timestamp ++ dat ++ prevHash
-        bytes = BC.pack content
-        hashed = hash bytes
-    in BC.unpack (B16.encode hashed)
-    
+difficulty :: Int
+difficulty = 1 -- should be 4 or higher
+
+calculateHash :: Int -> String -> String -> String -> Int -> Hash
+calculateHash index timestamp dat prevHash nonce =
+  let content = show index ++ timestamp ++ dat ++ prevHash ++ show nonce
+      bytes   = BC.pack content
+      hashed  = hash bytes
+  in BC.unpack (B16.encode hashed)
+
+-- No more shadowing, no more head
+mineBlock :: Int -> Int -> String -> String -> String -> (Hash, Int)
+mineBlock diff index timestamp dat prevHash =
+  go 0
+  where
+    go nonce
+      | take diff h == replicate diff '0' = (h, nonce)
+      | otherwise                          = go (nonce + 1)
+      where
+        h = calculateHash index timestamp dat prevHash nonce
+
 createGenesisBlock :: IO Block
 createGenesisBlock = do
-    now <- getCurrentTime
-    let timestamp = show now
-        prevHash = "0"
-        h = calculateHash 0 timestamp "Genesis Block" prevHash
-    return $ Block
-      { blockIndex = 0
-      , blockTimestamp = now
-      , blockData = "Genesis Block"
-      , blockPrevHash = prevHash
-      , blockHash = h
-      }
+  now <- getCurrentTime
+  let timestamp  = show now
+      prevHash   = "0"
+      dat        = "Genesis Block"
+      (h, nonce) = mineBlock difficulty 0 timestamp dat prevHash
+  return $ Block
+    { blockIndex     = 0
+    , blockTimestamp = now
+    , blockData      = dat
+    , blockPrevHash  = prevHash
+    , blockNonce     = nonce
+    , blockHash      = h
+    }
 
 addBlock :: Blockchain -> String -> IO Blockchain
 addBlock chain dat = do
-    now <- getCurrentTime
-    let prevBlock = last chain
-        newIndex = blockIndex prevBlock + 1
-        prevHash = blockHash prevBlock
-        timestamp = show now
-        h = calculateHash newIndex timestamp dat prevHash
-        newBlock = Block
-          { blockIndex = newIndex
-          , blockTimestamp = now
-          , blockData = dat
-          , blockPrevHash = prevHash
-          ,blockHash = h
-          }
-    return (chain ++ [newBlock])
+  now <- getCurrentTime
+  let prevBlock  = last chain
+      newIndex   = blockIndex prevBlock + 1
+      prevHash   = blockHash prevBlock
+      timestamp  = show now
+      (h, nonce) = mineBlock difficulty newIndex timestamp dat prevHash
+      newBlock   = Block
+        { blockIndex     = newIndex
+        , blockTimestamp = now
+        , blockData      = dat
+        , blockPrevHash  = prevHash
+        , blockNonce     = nonce
+        , blockHash      = h
+        }
+  return (chain ++ [newBlock])
+
+isValidHash :: Hash -> Bool
+isValidHash h = take difficulty h == replicate difficulty '0'
 
 isValidChain :: Blockchain -> Bool
-isValidChain [] = True
-isValidChain [_] = True
-isValidChain (b1:b2:rest) = blockHash b1 == blockPrevHash b2 && isValidChain (b2:rest)
+isValidChain []         = True
+isValidChain [b]        = isValidHash (blockHash b)
+isValidChain (b1:b2:rest) =
+     blockHash b1 == blockPrevHash b2
+  && isValidHash (blockHash b1)
+  && isValidChain (b2:rest)
--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,29 @@
+-- app/Main.hs
 module Main where
 
 import Blockchain
 
 main :: IO ()
 main = do
-  -- Start with genesis block
+  putStrLn "Mining genesis block..."
   genesis <- createGenesisBlock
-  let chain0 = [genesis]
 
-  -- Add some blocks
-  chain1 <- addBlock chain0 "Alice sends 10 coins to Bob"
+  putStrLn "Mining block 1..."
+  chain1 <- addBlock [genesis] "Alice sends 10 coins to Bob"
+
+  putStrLn "Mining block 2..."
   chain2 <- addBlock chain1 "Bob sends 5 coins to Carol"
-  chain3 <- addBlock chain2 "Carol sends 2 coins to Dave"
 
-  -- Print all blocks
+  chain3 <- addBlock chain2 "jihoo taken 1 coins from Carol"
+  
   mapM_ printBlock chain3
-
-  -- Validate
-  putStrLn $ "\nChain valid? " ++ show (isValidChain chain3)
+  putStrLn $ "\nChain valid? " ++ show (isValidChain chain2)
 
 printBlock :: Block -> IO ()
 printBlock b = do
   putStrLn $ replicate 50 '-'
   putStrLn $ "Index:     " ++ show (blockIndex b)
   putStrLn $ "Data:      " ++ blockData b
+  putStrLn $ "Nonce:     " ++ show (blockNonce b)   -- NEW
   putStrLn $ "Prev Hash: " ++ blockPrevHash b
   putStrLn $ "Hash:      " ++ blockHash b
